a10aa6d60b9fa33980ad222748bc0053d9726259
Niroshana De Silva <153500233+Niroshana-98@users.noreply.github.com>
2024-09-19 15:30:43 +0530

    upload photos

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
 
@@ -13,6 +17,7 @@ const jwtSecret ='wfiwiwinswtiwojtoarjapra';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173',
@@ -62,6 +67,7 @@ app.post('/login',async(req,res) => {
         res.json('Not Found');
     }
 });
+
 app.get('/profile', (req,res) => {
     const {token} = req.cookies;
     if (token) {
@@ -74,7 +80,29 @@ app.get('/profile', (req,res) => {
         res.json(null);
     }
 });
+
 app.post('/logout', (req, res) =>{
     res.cookie('token', '').json(true);
+});
+
+app.post('/upload-by-link', async(req,res) => {
+    const {link} = req.body;
+    const newName = 'photo' +Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' +newName,
+    });
+    res.json(newName);
+});
+
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload',photosMiddleware.array('photos',100),(req,res) =>{
+    for (let i = 0; i< req.files.length; i++){
+        const {path, originalname} = req.files[i];
+        const parts =originalname.split('');
+        const newPath = path;
+        fs.renameSync(path);
+    }
+    res.json(req.files);
 });
 app.listen(4003);
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import Perks from "../Perks";
+import axios from "axios";
 
 export default function PlacesPage(){
     const {action} = useParams();
@@ -35,6 +36,31 @@ export default function PlacesPage(){
             </>
         );
     }
+    
+    async function addPhotoByLink(ev){
+        ev.preventDefault();
+        const {data:filename} = await axios.post('/upload-by-link',{link: photoLink});
+        setAddedPhotos(prev => {
+            return[...prev, filename];
+        });
+        setPhotoLink('');
+    }
+
+    function uploadPhoto(ev){
+        const files = ev.target.files;
+        const data = new FormData();
+        for (let i = 0; i < files.length; i++){
+            data.append('photos',files[i]);
+        }
+        axios.post('/upload', data,{
+            headers: {'Content-type' : 'multipart/form-data'}
+        }).then(response =>{
+            const {data:filename} = response;
+            setAddedPhotos(prev => {
+                return[...prev, filename];
+            });
+        });
+    }
     return(
         <div>
             {action !== 'new' && (
@@ -60,15 +86,21 @@ export default function PlacesPage(){
                                    value={photoLink} 
                                    onChange={ev=> setPhotoLink(ev.target.value)} 
                                    placeholder="Add using a link  ....jpg"/>
-                            <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photo</button>
+                            <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photo</button>
                         </div>
-                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-                            <button className="flex justify-center gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                        <div className="mt-2 gap-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                            {addesPhotos.length > 0 && addesPhotos.map(link => (
+                                <div>
+                                    <img className="rounded-2xl" src={"http://localhost:4003/uploads/"+link}/>
+                                </div>
+                            ))}
+                            <label className=" cursor-pointer flex justify-center items-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+                                <input type="file" multiple className="hidden" onChange={uploadPhoto}/>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                                 </svg>
                                 Upload
-                            </button>
+                            </label>
                         </div>
                         {preInput('Description','description of the place')}
                         <textarea value={description} onChange={ev => setDescription(ev.target.value)}/>
